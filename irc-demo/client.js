7d0f092a7ce0b8d3c5fc4cab13d58914ad3e496d
TitanLi <lisheng0706@gmail.com>
2020-03-01 16:50:03 +0800

    Add irc client API

--- irc-demo/client.js
+++ irc-demo/client.js
@@ -1,16 +1,34 @@
 const irc = require('slate-irc');
 const net = require('net');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 const stream = net.connect({
-	    port: 6667,
-	    host: process.env.IRC_SERVER
+	port: 6667,
+	host: process.env.IRC_SERVER
 });
 
 const client = irc(stream);
-
-client.nick('Hackathon Taiwana');
-client.user('Taiwan', 'Taiwan');
-
 client.join('#HackathonTaiwan');
-client.send('#HackathonTaiwan', 'asd');
+
+app.get('/IRC', (req, res) => {
+	let user = req.query.user;
+	let message = req.query.message;
+	console.log(`${new Date()} ${user} => ${message}`)
+	if (message == undefined || user == undefined) {
+		res.status(400).send({ message: 'message or user not undefined' });
+	} else {
+		client.nick(`${user}`);
+		client.user('Taiwan', 'Taiwan');
+		client.send('#HackathonTaiwan', `${message}`);
+		res.send({ 'status': 'ok' });
+	}
+});
+
+app.listen(3000);
