4da0b24f30b51df76080ee6bad75713ae23d7b41
TitanLi <lisheng0706@gmail.com>
2020-03-02 11:20:57 +0800

    Update shell #2

--- shell/shell.sh
+++ shell/shell.sh
@@ -18,21 +18,21 @@ echo Step6 : Create .env file
 echo "IRC_SERVER = '127.0.0.1'" > .env
 echo Step7 : Copy BruneiHackaton.png
 cp Hankathon-Kit/logo/BruneiHackaton.png logo/BruneiHackaton.png
-echo Step8 : app.js
+echo Step8 : Copy app.js
 cp Hankathon-Kit/app.js app.js
-echo Step9 : App.qml
+echo Step9 : Copy App.qml
 cp Hankathon-Kit/App.qml App.qml
-echo Step10 : CountdownTimer.qml
+echo Step10 : Copy CountdownTimer.qml
 cp Hankathon-Kit/CountdownTimer.qml CountdownTimer.qml
-echo Step11 : IRC.qml
+echo Step11 : Copy IRC.qml
 cp Hankathon-Kit/IRC.qml IRC.qml
-echo Step12 : Logo.qml
+echo Step12 : Copy Logo.qml
 cp Hankathon-Kit/Logo.qml Logo.qml
-echo Step13 : package.json
+echo Step13 : Copy package.json
 cp Hankathon-Kit/package.json package.json
-echo Step14 : TimerPanel.qml
+echo Step14 : Copy TimerPanel.qml
 cp Hankathon-Kit/TimerPanel.qml TimerPanel.qml
-echo Step15 : Install library
+echo Step15 : Copy Install library
 npm install
 echo Step16 : Current working directory
 pwd
